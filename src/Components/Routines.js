9780fdf089caf33dcf57d1c23cd932ae3b87ae09
JacobCunico <Jacob.Cownico@gmail.com>
2023-06-21 15:43:06 -0600

    working on routines

--- src/Components/Routines.js
+++ src/Components/Routines.js
@@ -3,9 +3,17 @@ import React, { Fragment } from "react";
 function Routines({ routines }) {
 
     return (
-        <p>
-        <span>hello: {routines.name}</span>
-        </p>
+        <>
+        {
+            routines.map((routines) => {
+                return(
+                        <p>
+                        <span>hello: {routines.name}</span>
+                        </p>
+            )
+            })
+        }
+        </>
     )
 }
 
