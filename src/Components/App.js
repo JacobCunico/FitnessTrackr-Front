acfebc21110509ab4ae1f17560c680f39af71a9c
JacobCunico <Jacob.Cownico@gmail.com>
2023-06-21 15:03:14 -0600

    work on routines

--- src/Components/App.js
+++ src/Components/App.js
@@ -56,7 +56,7 @@ function App() {
             <Routes>
             <Route 
                     path='/' 
-                    element={<Routines />}
+                    element={<Routines routines={setRoutines}/>}
                 />
                 <Route 
                     path='/register' 
--- src/Components/Routines.js
+++ src/Components/Routines.js
@@ -3,26 +3,7 @@ import React, { Fragment } from "react";
 function Routines({ routines }) {
 
     return (
-    <>
-    {
-        routines && routines.map((routine) => {
-            return (
-                <Fragment key={routine._id}>
-                {
-                (
-                        <>
-                        <p style={{padding: "1px 40px", fontSize: "20px", flexFlow: "row wrap", border: "solid black" }}>
-                            <span style={{ fontWeight: "bold"}}> Title:</span> {routine.name}
-                            <span style={{ fontWeight: "bold"}}> Description:</span> {routine.description}</p>
-                        </>
-                    )
-
-                }
-                </Fragment>
-            )
-        })  
-    }
-    </>
+        <div>{routines.name}</div>
     );
 }
 
