731aede8eb470c0560a993426f791fed505239b3
JacobCunico <Jacob.Cownico@gmail.com>
2023-06-21 19:55:41 -0600

    routines renderedgit add .!

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -2,17 +2,45 @@
 //the componeents bascially interact with the practicle aplications of the ajax requests
 // alot of interacting with the token
 
-import { activitiesData } from "../Requests";
+import React from 'react';
 
-async function Activities() {
-    const results = await activitiesData();
-    if (results.success) {
+import { useEffect, useState } from 'react';
 
+const BASE_URL = `http://fitnesstrac-kr.herokuapp.com/api`
+const list = ['Routines', 'Activities', 'Users'];
+
+const fetchActivities = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/activities`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    return(
-        <h1>hello</h1>
-    )
-}
 
-export default Activities
+export default function Tabs() {
+    const [activities, setActivities] = useState([]);
+    useEffect(() => {
+      const getActivities = async () => {
+        const result = await fetchActivities();
+        setActivities(result);
+      };
+      getActivities();
+    }, []);
+    return (
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+        {activities.map((activity) => (
+          <div key={activity.id} style={{ marginLeft: '10px' }}>
+            <h2>{activity.name}</h2>
+            <p>{activity.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
--- src/Components/Routines.js
+++ src/Components/Routines.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { useEffect, useState } from 'react';
+import { routinesData } from "../Requests";
 
-
-function Routines({ routines }) {
-console.log("LOG FROM ROUTINE", routines);
+ function Routines() {
+    const [activities, setActivities] = useState([]);
+    useEffect(() => {
+      const getActivities = async () => {
+        const result = await routinesData();
+        setActivities(result);
+      };
+      getActivities();
+    }, []);
+    
     return (
-        <>
-        {
-            <p>
-                <p>HELLO WORLD</p>
-                {
-            routines.map((routine) => {
-                return(
-                        <p>
-                        <span>hello: {routine.name}</span>
-                        </p>
-                )
-            })
-        }
-            </p>
-        }
-        </>
-    )
-};
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+        {activities.map((activity) => (
+          <div key={activity.id} style={{ marginLeft: '10px' }}>
+            <h2>{activity.name}</h2>
+            <p>{activity.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
 export default Routines
